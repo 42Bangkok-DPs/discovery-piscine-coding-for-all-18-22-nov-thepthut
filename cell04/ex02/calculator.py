c5bcbcdae44cb1c7574fbb98a92b5fae6d8f9013
thepthut <67030296@kmitl.ac.th>
2024-11-22 08:49:41 +0000

    19

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+num = int(input("Give me the first number:"))
+num1 = int(input("Give me the second number:"))
+print("Thank you!")
+print(f"{num} + {num1} = {num+num1}")
+print(f"{num} - {num1} = {num-num1}")
+print(f"{num} / {num1} = {num/num1}")
+print(f"{num} x {num1} = {num*num1}")
